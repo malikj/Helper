d2316cdfc1d502217fdb2dddc9c2cfe036587b66
malikj <malikj@PRINHYLTPAP0637.local>
2018-06-09 15:01:38 +0530

    First commit

---
+++ Helper/Helper/Helper.h
@@ -0,0 +1,19 @@
+//
+//  Helper.h
+//  Helper
+//
+//  Created by malikj on 09/06/18.
+//  Copyright © 2018 malikj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Helper.
+FOUNDATION_EXPORT double HelperVersionNumber;
+
+//! Project version string for Helper.
+FOUNDATION_EXPORT const unsigned char HelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Helper/PublicHeader.h>
+
+
